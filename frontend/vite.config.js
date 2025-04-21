bf694ebdcbc4b65173d10e189c2eb6056f8c3718
Yashwanth-Chary-P <p.yashwanthchary@gmail.com>
2025-04-22 00:15:02 +0530

    Updating the vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: 'https://github.com/Yashwanth-Chary-P/Student-Management-System/',  // Ensure this is set correctly for deployment
+  build: {
+    outDir: 'dist',
+  }
+});
